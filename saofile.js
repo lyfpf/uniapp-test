fbe7266d74b6bf6ce7706e4aae8a478ce7465e4b
lyfpf <15016584603@163.com>
2020-08-25 14:02:53 +0800

    测试

--- saofile.js
+++ saofile.js
@@ -3,13 +3,19 @@ module.exports = {
     {
       type: 'input',
       name: 'key',
-      message: '百度统计key',
+      message: '百度统计key'
     }
   ],
   actions: [
     {
       type: 'add',
       files: '**',
+      templateDir: 'template/uniapp'
+    },
+    {
+      type: 'add',
+      files: '**',
+      templateDir: 'template/test'
     }
   ]
 }
--- template/uniapp/app.js
+++ template/uniapp/app.js
@@ -1,3 +1,3 @@
 module.exports = {
-  key: "<%= key -%>"
+  key2: "<%= key -%>"
 }
